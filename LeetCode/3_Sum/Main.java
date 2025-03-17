ad6470ce441042844078675e91f23ad2cc9bb428
Mona <jflotildes@utexas.edu>
2025-03-17 00:13:59 -0500

    3_Sum template

---
+++ LeetCode/3_Sum/Main.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String [] args) {
+        Solution solution = new Solution();
+
+
+  
+    }
+
+}
+
+class Solution {
+  /* 
+  Given an integer array nums, return all the triplets 
+  [nums[i], nums[j], nums[k]] such that 
+  1. i != j, i != k, and j != k, 
+  2. nums[i] + nums[j] + nums[k] == 0.
+
+  Notice that the solution set must not contain duplicate triplets.
+  Example 1:
+  Input: nums = [-1,0,1,2,-1,-4]
+  Output: [[-1,-1,2],[-1,0,1]]
+  Explanation: 
+  nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+  nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+  nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+  The distinct triplets are [-1,0,1] and [-1,-1,2].
+  Notice that the order of the output and the order of the triplets does not matter.
+  */
+  public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> threeSum = new ArrayList<List<Integer>>();
+
+
+
+
+        return threeSum;
+    
+    
+  }
+    
+}
