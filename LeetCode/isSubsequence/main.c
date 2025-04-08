33672fdf7ab323a34947ea2bc672857f1b7528e0
jarret <jflotildes@utexas.edu>
2025-04-08 00:47:52 -0500

    isSubsequence

---
+++ LeetCode/isSubsequence/main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include <string.h>
+/*
+Given two strings s and t, 
+return true if s is a subsequence of t, 
+or false otherwise.
+
+A subsequence of a string is a new string that is formed from the 
+original string by deleting some (can be none) of the characters 
+without disturbing the relative positions of the remaining characters. 
+(i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+*/
+ 
+bool isSubsequence(char* s, char* t);
+
+int main(int argc,char *args[]){
+
+
+
+
+
+    return 0;
+
+}
+
+
+bool isSubsequence(char* s, char* t){
+
+
+   return false;    
+}
